0890efd62e5ab496285b78566d4e4f0f8f4d7e98
Rushabh Nandanwar <rushabhnandanwar2005@gmail.com>
2026-02-26 18:13:13 +0530

    first commit

---
+++ Backend/src/Controllers/auth.controller.js
@@ -0,0 +1,60 @@
+import User from"../Models/user.model.js";
+import bcrypt from "bcrypt";
+import { generateToken } from "../lib/utils.js";
+
+export const signup = async (req, res) => {
+    try {
+        const {email, fullName, password} = req.body;
+
+        if(!email || !fullName || !password){
+            return res.status(400).json({message: "All fields are required"});
+        }
+        
+        if(password.length < 6){
+            return res.status(400).json({message: "Password must be at least 6 characters long"});
+        }
+
+        const user = await User.findOne({email});
+        if(user){
+            return res.status(400).json({message: "Email already exists"});
+        }
+
+        //Hashing Password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);   
+
+        const newUser = await User.create({
+            email,
+            fullName,
+            password: hashedPassword
+        })
+
+        if(newUser){
+            //JWT Token
+            generateToken(newUser._id, res);
+            await newUser.save();
+
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                email: newUser.email,
+                profilePicture: newUser.profilePicture,
+            })
+
+        } else {
+            return res.status(400).json({message: "Invalid credentials"});
+        }
+
+    } catch (error) {
+        return res.status(500).json({message: "Internal server error"});
+        console.log(error);
+    }
+};
+
+export const logout = (req, res) => {
+    res.send("Logout route");
+};
+
+export const login = (req, res) => {
+    res.send("Login route");
+};
---
+++ Backend/src/index.js
@@ -0,0 +1,21 @@
+import express from 'express'
+import authRoutes from './Routes/auth.route.js'
+import dotenv from 'dotenv'
+import { connectDB } from './lib/db.js'
+
+dotenv.config()
+const app = express()
+
+//Database Connection
+connectDB()
+
+//Middleware
+app.use(express.json())
+
+//API Routes
+app.use("/api/auth", authRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is runnig on PORT ${PORT}`)
+});
