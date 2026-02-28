85b26c0fdfd644e9591b378aa2304051f7a3d45c
Rushabh Nandanwar <rushabhnandanwar2005@gmail.com>
2026-02-28 16:32:57 +0530

    Connection setup added

--- Backend/src/Controllers/auth.controller.js
+++ Backend/src/Controllers/auth.controller.js
@@ -1,5 +1,5 @@
 import User from"../Models/user.model.js";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { generateToken } from "../lib/utils.js";
 
 export const signup = async (req, res) => {
